1cb263b66dc4b6e61ed8906281504d5a687025c7
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-07-02 18:28:11 +0530

    feat(SimpleSelect): enhance story structure and add various select examples for better demonstration

--- choreo-ui/workspaces/libs/design-system/src/components/SimpleSelect/SimpleSelect.stories.tsx
+++ choreo-ui/workspaces/libs/design-system/src/components/SimpleSelect/SimpleSelect.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { SimpleSelect } from './SimpleSelect';
 import React from 'react';
-import { Card } from '../Card';
-import { Box, SelectChangeEvent } from '@mui/material';
+import { Card, CardContent } from '../Card';
+import { Box, Typography } from '@mui/material';
 import { SelectMenuItem } from './SelectMenuItem/SelectMenuItem';
+import { SelectMenuSubHeader } from './SelectMenuSubHeader/SelectMenuSubHeader';
 
 const meta: Meta<typeof SimpleSelect> = {
   title: 'Components/SimpleSelect',
@@ -45,265 +46,180 @@ export const Default: Story = {
     size: 'small',
     disabled: false,
   },
-  render: function SimpleSelectDefault(args) {
+  render: function SimpleSelectDefault(_args) {
     const [age, setAge] = React.useState('20');
 
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
+    const handleChange = (
+      event:
+        | React.ChangeEvent<HTMLInputElement>
+        | (Event & { target: { value: unknown; name: string } })
+    ) => {
       setAge(event.target.value as string);
     };
-
-    return (
-      <Box>
-        <SimpleSelect
-          testId={`${testId}-story`}
-          value={age}
-          onChange={handleChange}
-          size={args.size}
-          disabled={args.disabled}
-        >
-          <SelectMenuItem testId="story-default-ten" value={10}>
-            Ten thousand only
-          </SelectMenuItem>
-          <SelectMenuItem testId="story-default-twenty" value={20}>
-            Twenty five thousand only
-          </SelectMenuItem>
-          <SelectMenuItem testId="story-default-thirty" value={30}>
-            Thirty thousand only
-          </SelectMenuItem>
-        </SimpleSelect>
-      </Box>
-    );
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    disabled: false,
-  },
-  render: function SimpleSelectSmall(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
     return (
       <Card testId={testId}>
-        <Box padding={3}>
-          <SimpleSelect
-            testId={`${testId}-small`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            disabled={args.disabled}
-          >
-            <SelectMenuItem testId="story-small-ten" value={10}>
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-small-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-small-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
-      </Card>
-    );
-  },
-};
-
-export const Medium: Story = {
-  args: {
-    size: 'medium',
-    disabled: false,
-  },
-  render: function SimpleSelectMedium(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
-    return (
-      <Card testId={testId}>
-        <Box padding={3}>
-          <SimpleSelect
-            testId={`${testId}-medium`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            disabled={args.disabled}
-          >
-            <SelectMenuItem testId="story-medium-ten" value={10}>
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-medium-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-medium-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
-      </Card>
-    );
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    disabled: true,
-    size: 'medium',
-  },
-  render: function SimpleSelectDisabled(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
-    return (
-      <Card testId={testId}>
-        <Box padding={3}>
-          <SimpleSelect
-            testId={`${testId}-disabled`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            disabled={args.disabled}
-          >
-            <SelectMenuItem testId="story-disabled-ten" value={10}>
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-disabled-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-disabled-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
-      </Card>
-    );
-  },
-};
-
-export const Error: Story = {
-  args: {
-    error: true,
-    size: 'medium',
-  },
-  render: function SimpleSelectError(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
-    return (
-      <Card testId={testId}>
-        <Box padding={3}>
-          <SimpleSelect
-            testId={`${testId}-error`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            error={args.error}
-            helperText="This is an error message"
-          >
-            <SelectMenuItem testId="story-error-ten" value={10}>
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-error-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-error-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
-      </Card>
-    );
-  },
-};
-
-export const Loading: Story = {
-  args: {
-    isLoading: true,
-    size: 'medium',
-  },
-  render: function SimpleSelectLoading(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
-    return (
-      <Card testId={testId}>
-        <Box padding={3}>
-          <SimpleSelect
-            testId={`${testId}-loading`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            isLoading={args.isLoading}
-          >
-            <SelectMenuItem testId="story-loading-ten" value={10}>
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-loading-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-loading-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
-      </Card>
-    );
-  },
-};
-
-export const Other: Story = {
-  args: {
-    size: 'medium',
-    disabled: false,
-  },
-  render: function SimpleSelectOther(args) {
-    const [age, setAge] = React.useState('20');
-
-    const handleChange = (event: SelectChangeEvent<unknown>) => {
-      setAge(event.target.value as string);
-    };
-
-    return (
-      <Card testId={testId}>
-        <Box padding={3} maxWidth={230}>
-          <SimpleSelect
-            testId={`${testId}-other`}
-            value={age}
-            onChange={handleChange}
-            size={args.size}
-            disabled={args.disabled}
-          >
-            <SelectMenuItem
-              testId="story-other-ten"
-              value={10}
-              description="This is a description for ten"
-            >
-              Ten thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-other-twenty" value={20}>
-              Twenty five thousand only
-            </SelectMenuItem>
-            <SelectMenuItem testId="story-other-thirty" value={30}>
-              Thirty thousand only
-            </SelectMenuItem>
-          </SimpleSelect>
-        </Box>
+        <CardContent>
+          <Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Size - Medium
+              </Typography>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+              >
+                <SelectMenuItem testId="story-medium-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-medium-twenty" value={20}>
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-medium-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Size - Small
+              </Typography>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+                size="small"
+              >
+                <SelectMenuItem testId="story-small-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-small-twenty" value={20}>
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-small-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                With Description
+              </Typography>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+              >
+                <SelectMenuItem testId="story-medium-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuItem
+                  testId="story-medium-twenty"
+                  value={20}
+                  description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                >
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-medium-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Others
+              </Typography>
+            </Box>
+            <Box mb={3} maxWidth={230}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+              >
+                <SelectMenuItem
+                  testId="story-max-ten"
+                  value={10}
+                  description="This is the first item description"
+                >
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-max-twenty" value={20}>
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-max-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+                error
+                helperText="This is an error"
+              >
+                <SelectMenuItem testId="story-error-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-error-twenty" value={20}>
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-error-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Grouping
+              </Typography>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={age}
+                onChange={handleChange}
+              >
+                <SelectMenuItem testId="story-medium-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+                <SelectMenuSubHeader testId="story-sub-header">
+                  Category 1
+                </SelectMenuSubHeader>
+                <SelectMenuItem testId="story-medium-twenty" value={20}>
+                  Twenty five thousand only
+                </SelectMenuItem>
+                <SelectMenuItem testId="story-medium-thirty" value={30}>
+                  Thirty thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Loading State
+              </Typography>
+            </Box>
+            <Box mb={3}>
+              <SimpleSelect
+                testId={`${testId}-story`}
+                value={null}
+                onChange={handleChange}
+                isLoading
+              >
+                <SelectMenuItem testId="story-medium-ten" value={10}>
+                  Ten thousand only
+                </SelectMenuItem>
+              </SimpleSelect>
+            </Box>
+          </Box>
+        </CardContent>
       </Card>
     );
   },
