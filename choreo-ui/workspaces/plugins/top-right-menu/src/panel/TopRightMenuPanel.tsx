0662fcdb65cdfbc607262bcf9463d1546d85fa26
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-09-16 20:49:56 +0530

    refactor: remove unused auth-core package and integrate thunder-auth-views
    - Removed the @open-choreo/auth-core package from rush.json and console package.json.
    - Updated App.tsx to include Login and Register routes from thunder-auth-views.
    - Refactored MainLayout to accommodate new routing structure.
    - Modified GlobalProviders to use ThunderConfig for authentication.
    - Updated Login and Register components to enhance UI with GridContainer and Box for better layout.
    - Adjusted storybook configurations for Login and Register components.
    - Cleaned up imports and ensured consistent formatting across files.

--- choreo-ui/workspaces/plugins/top-right-menu/src/panel/TopRightMenuPanel.tsx
+++ choreo-ui/workspaces/plugins/top-right-menu/src/panel/TopRightMenuPanel.tsx
@@ -32,7 +32,8 @@ const TopRightMenuPanel: React.FC = () => {
           }}
           checked={colorMode === "light"}
           color="primary"
-          size="small" />
+          size="small"
+        />
       </Box>
       {!user && <Button onClick={() => login()}>Sign In</Button>}
       {user && <Button onClick={() => logout()}>Sign Out</Button>}
