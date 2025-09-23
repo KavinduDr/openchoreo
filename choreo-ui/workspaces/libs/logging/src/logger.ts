6c839312b84b7e1284d5425b291d4901c904c2c5
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-09-23 09:54:09 +0530

    feat(logging): add logging dependency to multiple packages

--- choreo-ui/workspaces/libs/logging/src/logger.ts
+++ choreo-ui/workspaces/libs/logging/src/logger.ts
@@ -2,49 +2,49 @@ import { LOG_LEVEL } from './environment';
 
 // signature of loggin function
 export interface LogFn {
-    (message?: any, ...optionalParams: any[]): void;
+  (message?: any, ...optionalParams: any[]): void;
 }
 
 //basic logger interface
 export interface Logger {
-    log: LogFn;
-    warn: LogFn;
-    error: LogFn;
+  log: LogFn;
+  warn: LogFn;
+  error: LogFn;
 }
 
 //log levels
 export type LogLevel = 'log' | 'warn' | 'error';
 
-const NO_OP: LogFn = () => { };
+const NO_OP: LogFn = () => {};
 
 /** Logger which outputs to the browser console */
 export class ConsoleLogger implements Logger {
-    readonly log: LogFn;
-    readonly warn: LogFn;
-    readonly error: LogFn;
+  readonly log: LogFn;
+  readonly warn: LogFn;
+  readonly error: LogFn;
 
-    constructor(options?: { level?: LogLevel }) {
-        const { level } = options || {};
+  constructor(options?: { level?: LogLevel }) {
+    const { level } = options || {};
 
-        this.error = console.error.bind(console, '\n', new Error().stack);
+    this.error = console.error.bind(console, '\n', new Error().stack);
 
-        if (level === 'error') {
-            this.warn = NO_OP;
-            this.log = NO_OP;
+    if (level === 'error') {
+      this.warn = NO_OP;
+      this.log = NO_OP;
 
-            return;
-        }
-
-        this.warn = console.warn.bind(console, '\n', new Error().stack);
-
-        if (level === 'warn') {
-            this.log = NO_OP;
-
-            return;
-        }
-
-        this.log = console.log.bind(console, '\n', new Error().stack);
+      return;
     }
+
+    this.warn = console.warn.bind(console, '\n', new Error().stack);
+
+    if (level === 'warn') {
+      this.log = NO_OP;
+
+      return;
+    }
+
+    this.log = console.log.bind(console, '\n', new Error().stack);
+  }
 }
 
 export const logger = new ConsoleLogger({ level: LOG_LEVEL });
